f8a787df9f74e2d2dd0a97f94401c9e909ea5405
Leave <wangpeng@wangpengs-MacBook-Pro.local>
2020-06-23 11:12:34 +0800

    venn

--- VennPlot.pl
+++ VennPlot.pl
@@ -24,7 +24,7 @@ my $w = 1000;
 my $h = 1000;
 my $svg = SVG->new(width=>$w, height=>$h);
 my $pi = 4*atan2(1, 1);
-
+my $center_x = 500;
 
 my $num = scalar(@names);
 my @array = map{360/$num*($_-1)} (1..$num);
@@ -39,7 +39,7 @@ if($num >= 35){
 	$eclipse_rx = $eclipse_rx <= ($w - $r2*2)/4/2.5 ? $eclipse_rx : ($w - $r2*2)/4/2.5;
 	$eclipse_ry = $eclipse_rx * 2.5;
 }else{
-	$eclipse_ry = (400 - $r2-30)/2;
+	$eclipse_ry = ($center_x - $r2-30)/2;
 	$eclipse_rx = $eclipse_ry / 2.5;
 }
 
@@ -49,26 +49,26 @@ for my $n (0..$#names){
 	my $angle = $array[$n];
 	my $name = $names[$n];
 	my $num = $data{$name};
-	my $group=$svg->group('transform',"rotate($angle 400,400)", 'fill', 'orange');
-	$group->ellipse('cx',400,'cy',400-$eclipse_ry-$r2,'rx',$eclipse_rx,'ry',$eclipse_ry,'fill-opacity','0.5');
+	my $group=$svg->group('transform',"rotate($angle $center_x,$center_x)", 'fill', 'orange');
+	$group->ellipse('cx',$center_x,'cy',$center_x-$eclipse_ry-$r2,'rx',$eclipse_rx,'ry',$eclipse_ry,'fill-opacity','0.5');
 	my $nangle = n_angle($angle);
-	my $cx = 400 + sin($nangle)*($r2+$eclipse_ry*2);
-	my $cy = 400 - cos($nangle)*($r2+$eclipse_ry*2);
+	my $cx = $center_x + sin($nangle)*($r2+$eclipse_ry*2);
+	my $cy = $center_x - cos($nangle)*($r2+$eclipse_ry*2);
 	my $va = $angle <= 180 ? $angle - 90 : $angle + 90;
 	my $pos = text_position($angle);
 	$svg->text('x',$cx, 'y', $cy, '-cdata',"$name", 'text-anchor',$pos, 
 			'font-size', '12', 'transform', "rotate($va, $cx, $cy)");
 	
-	my $group2=$svg->group('transform',"rotate($angle 400,400)");
-	$group2->text('x',400, 'y',400-$eclipse_ry-$r2, '-cdata',"$num",
+	my $group2=$svg->group('transform',"rotate($angle $center_x,$center_x)");
+	$group2->text('x',$center_x, 'y',$center_x-$eclipse_ry-$r2, '-cdata',"$num",
 				  'text-anchor','middle', 'font-size', '12');
 	
 }
 
-$svg->circle('cx',400,'cy',400,'r',$r1,'fill','orange','fill-opacity', '1');
-$svg->circle('cx',400,'cy',400,'r',$r2,'fill','blue','fill-opacity', '0.5');
-$svg->text( 'x',400, 'y',400-$r2/1.5, '-cdata',"$mnms[0] ($data{$mnms[0]})", 'text-anchor','middle');
-$svg->text( 'x',400, 'y',400, '-cdata',"$mnms[1] ($data{$mnms[1]})", 'text-anchor','middle');
+$svg->circle('cx',$center_x,'cy',$center_x,'r',$r1,'fill','orange','fill-opacity', '1');
+$svg->circle('cx',$center_x,'cy',$center_x,'r',$r2,'fill','blue','fill-opacity', '0.5');
+$svg->text( 'x',$center_x, 'y',$center_x-$r2/1.5, '-cdata',"$mnms[0] ($data{$mnms[0]})", 'text-anchor','middle');
+$svg->text( 'x',$center_x, 'y',$center_x, '-cdata',"$mnms[1] ($data{$mnms[1]})", 'text-anchor','middle');
 
 
 my $out = $svg->xmlify;
